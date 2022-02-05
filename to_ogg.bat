d24d52e820579c8ebb3845f6cc1fc83ef18a7092
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2022-02-05 04:51:19 +0000

    Add files via upload

---
+++ to_ogg.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+for /f "delims=" %%f in ('dir /b/a-d "*.wem"') do (
+    ww2ogg.exe %%f --pcb packed_codebooks_aoTuV_603.bin
+
+)
+
+for /f "delims=" %%f in ('dir /b/a-d "*.ogg"') do (
+    revorb.exe %%f 
+)
+
+pause >nul
